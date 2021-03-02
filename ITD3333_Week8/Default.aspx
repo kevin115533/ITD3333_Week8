784c037fee2d55d7d8524b4f16aab54a48897d5e
Kevin Tran <kevin115533@yahoo.com>
2021-03-01 22:15:39 -0600

    Initial commit

---
+++ ITD3333_Week8/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label>Select Author: 
+            <asp:DropDownList ID="lastNameDropList" AutoPostBack="true" runat="server">
+            </asp:DropDownList>
+            </label><br /><br />
+            <asp:Label ID="resultLbl" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ITD3333_Week8/Default.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private string connectionString = WebConfigurationManager.ConnectionStrings["Pubs"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            FillAuthorList();
+        }
+    }
+
+    private void FillAuthorList()
+    {
+        lastNameDropList.Items.Clear();
+        string selectSQL = "SELECT au_lname, au_fname, au_id FROM Authors";
+
+        SqlConnection con = new SqlConnection(connectionString);
+        SqlCommand cmd = new SqlCommand(selectSQL, con);
+        SqlDataReader reader;
+
+        try
+        {
+            con.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ListItem newItem = new ListItem();
+                newItem.Text = reader["au_lname"] + ", " + reader["au_fname"];
+                newItem.Value = reader["au_id"].ToString();
+                lastNameDropList.Items.Add(newItem);
+            }
+            reader.Close();
+        }
+        catch (Exception err)
+        {
+            resultLbl.Text = "Error reading list of names. ";
+            resultLbl.Text += err.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    protected void lstAuthor_SelectedIndexChanged(Object sender, EventArgs e)
+    {
+        string selectSQL;
+        selectSQL = "SELECT * FROM Authors ";
+        selectSQL += "WHERE au_id='" + lastNameDropList.SelectedItem.Value + "'";
+        SqlConnection con = new SqlConnection(connectionString);
+        SqlCommand cmd = new SqlCommand(selectSQL, con);
+        SqlDataReader reader;
+        try
+        {
+            con.Open();
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<b>"); sb.Append(reader["au_lname"]);
+            sb.Append(", ");
+            sb.Append(reader["au_fname"]);
+            sb.Append("</b><br />");
+            sb.Append("Phone: ");
+            sb.Append(reader["phone"]);
+            sb.Append("<br />");
+            sb.Append("Address: ");
+            sb.Append(reader["address"]);
+            sb.Append("<br />");
+            sb.Append("City: ");
+            sb.Append(reader["city"]);
+            sb.Append("<br />");
+            sb.Append("State: ");
+            sb.Append(reader["state"]);
+            sb.Append("<br />");
+            
+            resultLbl.Text = sb.ToString();
+            reader.Close();
+        }
+        catch (Exception err)
+        {
+            resultLbl.Text = "Error getting author. ";
+            resultLbl.Text += err.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+}
